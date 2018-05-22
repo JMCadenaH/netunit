d7d939d8f5642ca1b9c92a983e33760ec732aafb
IngJavierR <hazelapd@gmail.com>
2018-05-22 15:39:15 -0500

    First commit

---
+++ RestProject/RestProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace RestProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var nancyHost = new NancyHost(new Uri("http://localhost:8888/"));
+
+            nancyHost.Start();
+
+            Console.WriteLine("Nancy is listening on http://localhost:8888/");
+            Console.ReadKey();
+
+            nancyHost.Stop();
+
+            Console.WriteLine("Nancy is stopped");
+        }
+    }
+}
---
+++ RestProject/RestProject/SampleModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace RestProject
+{
+    public class SampleModule : Nancy.NancyModule
+    {
+        public SampleModule(){
+
+            Get["/"] = _ => {
+                return "HelloWorld!";
+            };
+        }
+    }
+}
